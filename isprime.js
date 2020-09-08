4d7ed2e988653cc04d7ddbd5dbc415b32c71a395
martinbanden <martinbanden@gmail.com>
2020-09-08 12:52:56 +0200

    blah

--- isprime.js
+++ isprime.js
@@ -1,11 +1,19 @@
 "use strict"
 
-const isPrime = function (arg) {
-    for (var i = 2; i < arg; i += 1) {
-        if (arg % i === 0) {
-            return false;
-        }
-    }
-    return true;
-}
+let n;
+const isPrime = function (prime) {
 
+	    for (var i = 3; i < math.sqrt(arg); i += 2) {
+	        if (prime % i === 0) {
+	            return n = false;
+	        	}
+	        return n = true;
+	    }
+if n = true
+	document.write ( "dette er et")
+
+let start = new Date();
+let b = isPrime(prime);
+let stop = new Date();
+let elapsed = stop - start;
+}
