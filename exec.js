c05870d8bb257c40f7d9dbe53356b7eea9d29d2e
Lotte <lotteravn@live.dk>
2020-09-08 12:16:10 +0200

    exec.js funcion for receiving integer

---
+++ exec.js
@@ -0,0 +1,22 @@
+"Use strict"
+
+function getPrime(){
+let prime;
+prime = Number(window.prompt('Please enter a integer of max 9007195909437503'));
+return prime;
+
+}
+
+
+/*testcode
+"Use strict"
+getPrime();
+
+function getPrime(){
+let prime;
+prime = Number(window.prompt('Please enter a integer of max 9007195909437503'));
+return prime;
+document.write(prime);
+}
+*/
+
